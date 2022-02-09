2f71fd787d107ff1b00559c5f0fd14284a55e12b
Anastasiya <Anastasiya@sergeys-mbp.kpn>
2022-02-09 19:03:50 +0100

    fix: add special cases

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+  if(matrix === undefined) return [];
+  if(matrix.length === 0) return [];
   let newMatrix = matrix.map ( (item, index) => {
     if(index % 2 === 0) {
         return item.sort( (a, b) => a - b );
@@ -9,5 +11,6 @@ module.exports = function towelSort (matrix) {
         return item.sort( (a, b) => b - a);
     }
   });
+  newMatrix.flat()
   return newMatrix.flat();
 }
