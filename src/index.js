1cfb758133251e248ee762c2b057e7a0bdc51806
Anastasiya <Anastasiya@sergeys-mbp.kpn>
2022-02-09 18:50:45 +0100

    feat: add script for towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let newMatrix = matrix.map ( (item, index) => {
+    if(index % 2 === 0) {
+        return item.sort( (a, b) => a - b );
+    } else {
+        return item.sort( (a, b) => b - a);
+    }
+  });
+  return newMatrix.flat();
 }
